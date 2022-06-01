08d6d3968441beb1e9d84e7bd9fd42042e3144ee
信诚 冯 <1143734409@qq.com>
2022-06-01 22:42:42 +0800

    作业

--- hellocoord.cpp
+++ hellocoord.cpp
@@ -239,16 +239,16 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-    //else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-    //{
-    //    mixValue += 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
-    //    if (mixValue >= 1.0f)
-    //        mixValue = 1.0f;
-    //}
-    //else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-    //{
-    //    mixValue -= 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
-    //    if (mixValue <= 0.0f)
-    //        mixValue = 0.0f;
-    //}
+    else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        mixValue += 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        if (mixValue >= 1.0f)
+            mixValue = 1.0f;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        mixValue -= 0.001f; // change this value accordingly (might be too slow or too fast based on system hardware)
+        if (mixValue <= 0.0f)
+            mixValue = 0.0f;
+    }
 }
